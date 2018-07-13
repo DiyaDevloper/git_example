009f2dd50ce91d367c78cb947baeca88552b1805
Kartik Jasolia <kartikjasolia@kartiks-imac.local>
2018-07-13 16:27:28 +0530

    add button that print message in console

--- git_example/ViewController.swift
+++ git_example/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func touchmeifyoucan(_ sender: Any) {
+        print("this is git hub example")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
