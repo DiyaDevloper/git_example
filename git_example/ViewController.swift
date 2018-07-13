afc9f06fb74454a36a72715d9d6727151d6f1718
Kartik Jasolia <kartikjasolia@kartiks-imac.local>
2018-07-13 16:07:05 +0530

    Initial Commit

---
+++ git_example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_example
+//
+//  Created by Kartik Jasolia on 13/07/18.
+//  Copyright © 2018 Kartik Jasolia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
