5f18b0af6aa3b2d9cf149f22abdf10611e8413d5
Kartik Jasolia <kartikjasolia@kartiks-imac.local>
2018-07-13 16:35:24 +0530

    create VSObject Class name MApForItinerary

---
+++ git_example/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  git_example
+//
+//  Created by Kartik Jasolia on 13/07/18.
+//  Copyright © 2018 Kartik Jasolia. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
